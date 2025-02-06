2905349dd024ee26ca881db90f36d2086a44f47f
norwich <NorwichSnower@proton.me>
2025-02-06 16:43:09 +0800

    DeployPoolManager

--- script/DeployPositionManager.s.sol
+++ script/DeployPositionManager.s.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
-import {PoolManager} from "../src/PoolManager.sol";
+import {PositionManager} from "../src/PositionManager.sol";
 
 contract DepolyPoolManger is Script {
     address public ANVIL_ACCOUNT = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
     address public MAINNET_ACCOUNT = 0x67612F0D87a3A6bBc13074Bf54c0500dbA12f4D4;
     address public SEPOLIA_ACCOUNT = 0x67612F0D87a3A6bBc13074Bf54c0500dbA12f4D4;
-    function run() external returns (PoolManager poolManager) {
+    function run() external returns (PositionManager positionManager) {
         address account;
         if (block.chainid == 1) {
             account = MAINNET_ACCOUNT;
@@ -19,7 +19,7 @@ contract DepolyPoolManger is Script {
         }
 
         vm.startBroadcast(account);
-        poolManager = new PoolManager();
+        positionManager = new PositionManager();
         vm.stopBroadcast();
     }
 }
